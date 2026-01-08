7c822c83d743c04ba5e9257fc97a29bfe76e0a4c
ali-hue-byte <elguelouiali@gmail.com>
2026-01-08 20:03:27 +0100

    Enhance MainWindow with file input and copy features
    Added file input and copy functionality to encrypt and decrypt text. Improved UI animations and error handling for password validation.

--- EncodeX/MainWindow.xaml.cs
+++ EncodeX/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace EncodeX
 {
@@ -27,7 +28,7 @@ namespace EncodeX
         Brush color = Brushes.Green;
         public MainWindow()
         {
-            
+
             InitializeComponent();
             Border_haha.MouseEnter += (s, e) =>
             {
@@ -40,6 +41,8 @@ namespace EncodeX
                 animation(Border_decrypt);
                 animation(decrypt_btn);
                 animation(arrow);
+                animation(File_input);
+                animation(Select_file);
                 DoubleAnimation animX = new DoubleAnimation
                 {
                     From = 389,
@@ -59,6 +62,8 @@ namespace EncodeX
                 animation2(Border_decrypt, "98,24,114,192");
                 animation2(decrypt_btn, "319,223,71,66");
                 animation2(arrow, "328,2,80,246");
+                animation2(File_input, "34,37,33,310");
+                animation2(Select_file, "120,57,120,232");
                 DoubleAnimation animX = new DoubleAnimation
                 {
                     From = 489,
@@ -68,18 +73,18 @@ namespace EncodeX
                 errorLabel.BeginAnimation(Canvas.LeftProperty, animX);
             };
         }
-        public void animation(FrameworkElement thing) 
+        public void animation(FrameworkElement thing)
         {
             Thickness current = thing.Margin;
             ThicknessAnimation animation = new ThicknessAnimation
             {
                 From = current,
-                To = new Thickness(current.Left+100, current.Top, current.Right-100, current.Bottom),
+                To = new Thickness(current.Left + 100, current.Top, current.Right - 100, current.Bottom),
                 Duration = TimeSpan.FromMilliseconds(300),
             };
             thing.BeginAnimation(FrameworkElement.MarginProperty, animation);
         }
-        public void animation2(FrameworkElement thing , string end)
+        public void animation2(FrameworkElement thing, string end)
         {
             string[] strings = end.Split(',');
             Thickness End = new Thickness(double.Parse(strings[0]), double.Parse(strings[1]), double.Parse(strings[2]), double.Parse(strings[3]));
@@ -116,7 +121,7 @@ namespace EncodeX
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+
             string password = password_field.Text;
             string plainText = input_field.Text;
 
@@ -133,7 +138,7 @@ namespace EncodeX
 
             }
 
-            
+
             else if (password.Length < 8 || password == "")
             {
                 errorLabel.Content = "Password must be at least 8 characters long";
@@ -177,6 +182,8 @@ namespace EncodeX
                 Border_pss.BorderBrush = color;
 
             }
+            SolidColorBrush newBrush = new SolidColorBrush(Colors.Green);
+            arrowProgress.Fill = newBrush;
             arrowProgress.Width = 0;
             arrowTranslate.X = 0;
             DoubleAnimation widthAnim = new DoubleAnimation
@@ -193,13 +200,13 @@ namespace EncodeX
 
                 DoubleAnimation posAnim = new DoubleAnimation
                 {
-                    From = 0,   
-                    To = -20,   
+                    From = 0,
+                    To = -20,
                     Duration = TimeSpan.FromMilliseconds(100),
                     FillBehavior = FillBehavior.Stop
                 };
 
-                
+
 
                 posAnim.Completed += (s2, e2) =>
                 {
@@ -221,15 +228,15 @@ namespace EncodeX
                     arrowProgress.BeginAnimation(FrameworkElement.WidthProperty, resetAnim);
                     arrowTranslate.BeginAnimation(TranslateTransform.XProperty, moveAnim2);
 
-                    
-                        encrypted_field.Text = encrypt(password, plainText);
-                    
+
+                    encrypted_field.Text = encrypt(password, plainText);
+
 
 
                     arrowTranslate.X = -20;
 
                     arrowProgress.Width = 0;
-                    
+
 
 
 
@@ -245,16 +252,20 @@ namespace EncodeX
 
 
 
-            
+
         }
 
-        
+
 
         private void decrypt_txt(object sender, RoutedEventArgs e)
         {
+
             string password = password_field.Text;
             string plainText = encrypted_field.Text;
-
+            if (plainText == "")
+            {
+                return;
+            }
             if (password_field.IsReadOnly == true)
             {
                 errorLabel.Visibility = Visibility.Hidden;
@@ -276,7 +287,70 @@ namespace EncodeX
                 Border_pss.BorderBrush = color;
 
             }
-            input_field.Text = decrypt(password, plainText);
+            SolidColorBrush newBrush = new SolidColorBrush(Colors.DodgerBlue);
+            arrowProgress.Fill = newBrush;
+            arrowProgress.Width = 0;
+            arrowTranslate.X = 0;
+            DoubleAnimation widthAnim = new DoubleAnimation
+            {
+                From = 0,
+                To = 20,
+                Duration = TimeSpan.FromMilliseconds(200),
+                FillBehavior = FillBehavior.Stop
+            };
+
+            widthAnim.Completed += (s1, e1) =>
+            {
+                arrowProgress.Width = 20;
+
+                DoubleAnimation posAnim = new DoubleAnimation
+                {
+                    From = 0,
+                    To = -20,
+                    Duration = TimeSpan.FromMilliseconds(100),
+                    FillBehavior = FillBehavior.Stop
+                };
+
+
+
+                posAnim.Completed += (s2, e2) =>
+                {
+                    DoubleAnimation resetAnim = new DoubleAnimation
+                    {
+                        From = 20,
+                        To = 0,
+                        Duration = TimeSpan.FromMilliseconds(100),
+                        FillBehavior = FillBehavior.Stop
+                    };
+                    DoubleAnimation moveAnim2 = new DoubleAnimation
+                    {
+                        From = -20,
+                        To = -70,
+                        Duration = TimeSpan.FromMilliseconds(150),
+                        FillBehavior = FillBehavior.Stop
+                    };
+
+                    arrowProgress.BeginAnimation(FrameworkElement.WidthProperty, resetAnim);
+                    arrowTranslate.BeginAnimation(TranslateTransform.XProperty, moveAnim2);
+
+
+                    input_field.Text = decrypt(password, plainText);
+
+
+
+                    arrowTranslate.X = -20;
+
+                    arrowProgress.Width = 0;
+
+
+
+
+                };
+
+                arrowTranslate.BeginAnimation(TranslateTransform.XProperty, posAnim);
+            };
+
+            arrowProgress.BeginAnimation(FrameworkElement.WidthProperty, widthAnim);
         }
         public string encrypt(string password, string text)
         {
@@ -301,14 +375,14 @@ namespace EncodeX
                 {
                     ms.Write(salt, 0, salt.Length);
                     ms.Write(iv, 0, iv.Length);
-                    using(CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
                     {
                         byte[] plainBytes = Encoding.UTF8.GetBytes(text);
                         cs.Write(plainBytes, 0, plainBytes.Length);
                     }
                     encrypted = Convert.ToBase64String(ms.ToArray());
                 }
-                
+
                 ;
             }
             return encrypted;
@@ -324,7 +398,7 @@ namespace EncodeX
 
             try
             {
-                
+
                 byte[] encrypted = Convert.FromBase64String(text);
                 byte[] salt = new byte[16];
                 byte[] iv = new byte[16];
@@ -351,14 +425,25 @@ namespace EncodeX
                 decrypted = "";
                 errorLabel.Content = "Decryption failed. Check your input and try again.";
                 errorLabel.Visibility = Visibility.Visible;
-                
+
             }
 
-                return decrypted;
+            return decrypted;
+        }
+        public void opacity_anim(Button button, double x, double y)
+        {
+            DoubleAnimation opacity = new DoubleAnimation
+            {
+                From = x,
+                To = y,
+                Duration = TimeSpan.FromMilliseconds(400)
+            };
+            button.BeginAnimation(Button.OpacityProperty, opacity);
         }
 
         public void decrypt_clicked(object sender, RoutedEventArgs e)
         {
+            errorLabel.Visibility = Visibility.Hidden;
             if (button_Decrypt.Tag as String == "Clicked")
             {
                 return;
@@ -382,11 +467,17 @@ namespace EncodeX
                 Duration = TimeSpan.FromMilliseconds(400)
             };
 
-
+            
+            opacity_anim(Copy,1.0,0.0);
+            opacity_anim(Copy2, 0.0, 1.0);
+            Copy2.Visibility= Visibility.Visible;
             button_Decrypt.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
             button_Encrypt.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, text_anim2);
             Border_encrypt.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
             Border_decrypt.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
+            File_input.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
+            Select_file.Background.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
+            Select_file.Tag = "Blue";
             Border_haha.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
             DropShadowEffect effect2 = Border_haha.Effect as DropShadowEffect;
             ColorAnimation dropShadowAnim = new ColorAnimation
@@ -399,7 +490,7 @@ namespace EncodeX
             {
                 Anim(1.0, 0.0, encrypt_btn, decrypt_btn);
                 Anim(0.0, 1.0, decrypt_btn, decrypt_btn);
-                
+
 
             }
             if (password_field.IsReadOnly == false)
@@ -411,7 +502,7 @@ namespace EncodeX
 
                 Border_pss.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
                 errorLabel.Visibility = Visibility.Hidden;
-                
+
 
 
             }
@@ -421,7 +512,7 @@ namespace EncodeX
             encrypted_field.IsReadOnly = false;
             button_password.Tag = "Locked";
             arrow.RenderTransform = new RotateTransform(0);
-            
+
             arrow.RenderTransformOrigin = new Point(0.5, 0.5);
 
             DoubleAnimation rotate = new DoubleAnimation
@@ -446,17 +537,18 @@ namespace EncodeX
                 To = y,
                 Duration = TimeSpan.FromMilliseconds(400)
             };
-            if (final != decrypt_btn) {
+            if (final != decrypt_btn)
+            {
                 anim.Completed += (s, ev) =>
                 {
                     decrypt_btn.Visibility = Visibility.Hidden;
                 };
             }
-            
+
 
             target.BeginAnimation(Button.OpacityProperty, anim);
             final.Visibility = Visibility.Visible;
-            
+
         }
         public void Anim2(TextBox textbox)
         {
@@ -481,6 +573,7 @@ namespace EncodeX
         }
         public void encrypt_clicked(object sender, RoutedEventArgs e)
         {
+            errorLabel.Visibility = Visibility.Hidden;
             Random random = new Random();
             int[] options = { 180, 540 };
             int choice = options[random.Next(options.Length)];
@@ -505,10 +598,16 @@ namespace EncodeX
                 To = Colors.Gray,
                 Duration = TimeSpan.FromMilliseconds(400)
             };
+            opacity_anim(Copy2, 1.0, 0.0);
+            opacity_anim(Copy, 0.0, 1.0);
+            Copy.Visibility = Visibility.Visible;
             button_Decrypt.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, text_anim);
             button_Encrypt.Foreground.BeginAnimation(SolidColorBrush.ColorProperty, text_anim2);
             Border_encrypt.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim2);
             Border_decrypt.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim2);
+            File_input.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim2);
+            Select_file.Background.BeginAnimation(SolidColorBrush.ColorProperty, text_anim2);
+            Select_file.Tag = "Green";
             Border_haha.BorderBrush.BeginAnimation(SolidColorBrush.ColorProperty, text_anim2);
             DropShadowEffect effect1 = Border_haha.Effect as DropShadowEffect;
             ColorAnimation dropShadowAnim = new ColorAnimation
@@ -522,7 +621,7 @@ namespace EncodeX
             {
                 Anim(1.0, 0.0, decrypt_btn, encrypt_btn);
                 Anim(0.0, 1.0, encrypt_btn, encrypt_btn);
-                
+
 
 
             }
@@ -556,10 +655,60 @@ namespace EncodeX
 
         private void button_files_Click(object sender, RoutedEventArgs e)
         {
-            input_field.Visibility = Visibility.Hidden;
-            Select_file.Visibility= Visibility.Visible;
-            
+            input_field.IsReadOnly = true;
+            Border_encrypt.Visibility = Visibility.Hidden;
+            Select_file.Visibility = Visibility.Visible;
+            File_input.Visibility = Visibility.Visible;
+            Border border_files = (Border)button_files.Template.FindName("border_files", button_files);
+            Border border = (Border)Button_text.Template.FindName("border", Button_text);
+            border_files.BorderThickness = new Thickness(3);
+            border.BorderThickness = new Thickness(1);
 
+        }
+        private void button_Text_Click(object sender, RoutedEventArgs e)
+        {
+            input_field.IsReadOnly = false;
+            Border_encrypt.Visibility = Visibility.Visible;
+            Select_file.Visibility = Visibility.Hidden;
+            File_input.Visibility = Visibility.Hidden;
+            Border border_files = (Border)button_files.Template.FindName("border_files", button_files);
+            Border border = (Border)Button_text.Template.FindName("border", Button_text);
+            border_files.BorderThickness = new Thickness(1);
+            border.BorderThickness = new Thickness(3);
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(encrypted_field.Text);
+            ToolTip tt = new ToolTip
+            {
+                Content = "Copied to Clipboard!",
+                IsOpen = true,
+                StaysOpen = false,
+                PlacementTarget = Copy,
+                Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom,
+                Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#1AAE1A")),
+                Foreground= Brushes.White,
+                FontFamily = new FontFamily("Arial Black"),
+                FontSize = 11
+            };
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(input_field.Text);
+            ToolTip tt = new ToolTip
+            {
+                Content = "Copied to Clipboard!",
+                IsOpen = true,
+                StaysOpen = false,
+                PlacementTarget = Copy2,
+                Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom,
+                Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2563EB")),
+                Foreground = Brushes.White,
+                FontFamily = new FontFamily("Arial Black"),
+                FontSize = 11
+            };
         }
     }
 }
